ed7f88332c656de291cf4f9011c6a16bba1a2a5f
adzanidham <adzani234@gmail.com>
2021-04-22 13:36:15 +0700

    '02Variable'

---
+++ src/02Variable.kt
@@ -0,0 +1,10 @@
+fun main() {
+  var myVariable = 5
+  println("The value of our variable is: $myVariable")
+  myVariable = 2
+  println("The value of our variable is now: $myVariable")
+
+  var favNum = 27
+  var age = 25
+  println("My favorite number is $favNum, and I'am $age years old.")
+}
